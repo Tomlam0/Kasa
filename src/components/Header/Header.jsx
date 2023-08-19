c419c89a27661eabd313949242aca9bc2a592a7d
Droubi1 <105873915+Droubi1@users.noreply.github.com>
2023-08-19 16:04:21 +0200

    style: Add responsive for header component

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -2,17 +2,14 @@ import React from "react";
 
 import { Link, NavLink } from "react-router-dom";
 
+import logo from "../../assets/img/logo.png";
+
 const Header = () => {
     return (
         <div id="header">
-            <div className="logo">
-                <Link to="/">
-                    <img
-                        src={require("../../assets/img/logo.png")}
-                        alt="logo kasa"
-                    />
-                </Link>
-            </div>
+            <Link to="/">
+                <img src={logo} className="logo" alt="logo kasa" />
+            </Link>
 
             <nav>
                 <ul>
