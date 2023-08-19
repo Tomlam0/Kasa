64bfdabfa7f6ba18696722527eeef2e0296c537d
Droubi1 <105873915+Droubi1@users.noreply.github.com>
2023-08-19 12:31:40 +0200

    style: Added Header componant

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,23 +1,43 @@
 import React from "react";
 
+import { Link, NavLink } from "react-router-dom";
 
 const Header = () => {
     return (
         <div id="header">
             <div className="logo">
-                <img
-                    src={require("../../assets/img/logo.png")}
-                    alt="logo kasa"
-                />
+                <Link to="/">
+                    <img
+                        src={require("../../assets/img/logo.png")}
+                        alt="logo kasa"
+                    />
+                </Link>
             </div>
 
             <nav>
                 <ul>
-                    <li></li>
-                    <li></li>
+                    <li>
+                        <NavLink
+                            to="/"
+                            className={({ isActive }) =>
+                                isActive ? "active" : ""
+                            }
+                        >
+                            Accueil
+                        </NavLink>
+                    </li>
+                    <li>
+                        <NavLink
+                            to="/about"
+                            className={({ isActive }) =>
+                                isActive ? "active" : ""
+                            }
+                        >
+                            A Propos
+                        </NavLink>
+                    </li>
                 </ul>
             </nav>
-
         </div>
     );
 };
