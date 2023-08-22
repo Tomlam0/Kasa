9c204341751c23e61e8dfc7dd898ea33eaae8ba0
Droubi1 <105873915+Droubi1@users.noreply.github.com>
2023-08-22 10:08:51 +0200

    style: Switching picture and text for Banner

--- src/components/Banner/Banner.jsx
+++ src/components/Banner/Banner.jsx
@@ -1,13 +1,34 @@
 import React from "react";
 
+import { useLocation } from "react-router-dom";
+
 import bannerHome from "../../assets/img/banner-home.webp";
+import bannerAbout from "../../assets/img/banner-about.webp";
 
 const Banner = () => {
-    const text = "Chez vous,\n partout et ailleurs";
+    const location = useLocation();
+    const currentPath = location.pathname;
+
+    let text = ""; // Définit vide pour qu'il soit déclaré uniquement dans Home par la suite
+    let bannerImage = null;
+
+    // Condition pour savoir quelle banière afficher en fonction de la route
+    if (currentPath === "/") {
+        bannerImage = bannerHome;
+        text = "Chez vous,\n partout et ailleurs";
+    } else if (currentPath === "/about") {
+        bannerImage = bannerAbout;
+    } else {
+        bannerImage = bannerHome;
+    }
 
     return (
         <section id="banner">
-            <img src={bannerHome} alt="bannière accueil" />
+            <img
+                src={bannerImage}
+                alt="bannière accueil paysage"
+                className={currentPath === "/about" ? "about-banner" : ""} // Ajoute une class uniquement pour la banner du path about
+            />
             <p>{text}</p>
         </section>
     );
