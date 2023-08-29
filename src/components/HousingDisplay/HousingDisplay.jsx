40d0ffb155343448f75c4c67164d410655ecb2da
Droubi1 <105873915+Droubi1@users.noreply.github.com>
2023-08-29 09:57:33 +0200

    featre: collapse can now find specific url in HousingDisplay

--- src/components/HousingDisplay/HousingDisplay.jsx
+++ src/components/HousingDisplay/HousingDisplay.jsx
@@ -1,5 +1,6 @@
 import React from "react";
-// import { useParams } from "react-router-dom";
+
+import { useParams } from "react-router-dom";
 
 import locationData from "../../assets/data/logements.json";
 
@@ -9,10 +10,10 @@ import Collapse from "../Collapse/Collapse";
 import styles from "./housingDisplay.module.scss";
 
 const HousingDisplay = () => {
-    //mise a 0 pour le moment pour les tests
-    const data = locationData[0];
     // Pour trouver l'id du logement en question
-    // const { id } = useParams();
+    // useParams retourne un objet contenant les paramètres de l'URL actuelle
+    const { id } = useParams();
+    const data = locationData.find((location) => location.id === id);
 
     return (
         <>
